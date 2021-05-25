1b15d48ef5f5636758c11574b9fcac9e072474d1
Enrique <e.garciagranda@gmail.com>
2021-05-26 00:41:48 +0200

    Animations almost done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float MovX;
     public float JumpForce, Speed;
-    //private bool IsOnGround;
+
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,15 +22,15 @@ public class PlayerMovement : MonoBehaviour
     {
         MovX = Input.GetAxisRaw("Horizontal");
 
+        LeftOrRight(MovX);
+        Animator.SetBool("isRunning", MovX != 0);
+
         if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
         {
+            Animator.SetTrigger("jump");
             Rigidbody2D.AddForce(Vector2.up * JumpForce);
+            
         }
-    }
-
-    private void Jump()
-    {
-
     }
 
     private void FixedUpdate()
@@ -36,14 +38,30 @@ public class PlayerMovement : MonoBehaviour
         Rigidbody2D.velocity = new Vector2(MovX * Speed, Rigidbody2D.velocity.y);
     }
 
+    /**
+     * Checks if the player is moving left or right
+     */
+    private void LeftOrRight(float AxisX)
+    {
+        if (AxisX < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else if (AxisX > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+    }
+
+    /**
+     * Checks if the player is on the ground
+     */
     private bool IsOnGround()
     {
-        if(Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.5f))
         {
             return true;
         }
         return false;
     }
-
-
 }
